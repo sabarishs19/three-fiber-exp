e046b3579e73635d7fb87e9d4f2c55828411135b
sabarishs19 <84761863+sabarishs19@users.noreply.github.com>
2021-11-30 18:53:08 +0530

    u

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import "./styles.css";
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three";
-import { Circle, OrbitCountrols, Environment } from "@react-three/drei";
+import { Circle, OrbitControls, Environment } from "@react-three/drei";
 import usePostprocessing from "./shaders/usePostprocessing";
 import useReflector from "./shaders/useReflector";
 import Sphere from "./Sphere";
 import "./shaders/materials/ReflectorMaterial";
 
-function Modal(props) {
+function Model(props) {
   const [meshRef, reflectorProps, passes] = useReflector();
   usePostprocessing(passes);
   return (
@@ -35,9 +35,32 @@ function Modal(props) {
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Canvas
+      gl={{ alpha: false }}
+      shadwows
+      camera={{ position: [0, 0, 20], fov: 15, near: 1, far: 50 }}
+    >
+      <color attach="background" args={["#151515"]} />
+      <fog attach="fog" args={["#151515", 20, 30]} />
+      <ambientLight intensity={0.4} />
+      <spotLight
+        intensity={10}
+        penumbra={1}
+        angle={0.2}
+        position={[10, 10, 10]}
+        castShadow
+      />
+      <Suspense fallback={null}>
+        <Model position={[0, -1, 0]} rotation={[-0.4, 0, 0]} />
+        <Environment files="adams_place_bridge_1k.hdr" />
+      </Suspense>
+      <OrbitControls
+        autoRotate
+        enableZoom={false}
+        enablePan={false}
+        minPolarAngle={Math.PI / 3}
+        maxPolarAngle={Math.PI / 3}
+      />
+    </Canvas>
   );
 }
