8ebff92958561caf725fa9f5b466f58456809ce5
sabarishs19 <84761863+sabarishs19@users.noreply.github.com>
2021-11-30 14:50:26 +0530

    states

--- src/shaders/useReflector.js
+++ src/shaders/useReflector.js
@@ -1,7 +1,6 @@
 import { useCallback, useMemo, useState, useRef } from "react";
 import * as THREE from "THREE";
 import { SavePass, RenderPass, LambdaPass, BlurPass } from "postprocessing";
-
 import { useThree } from "@react-three/fiber";
 
 export default function useReflector(textureWidth = 128, textureHeight = 128) {
@@ -12,4 +11,16 @@ export default function useReflector(textureWidth = 128, textureHeight = 128) {
   const [cameraWorldPosition] = useState(() => new THREE.Vector3());
   const [rotationMatrix] = useState(() => new THREE.Vector3());
   const [lookAtPosition] = useState(() => new THREE.Vector3(0, 0, -1));
+  const [clipPlane] = useState(() => new THREE.Vector4());
+  const [view] = useState(() => new THREE.Vector3());
+  const [target] = useState(() => new THREE.Vector3());
+  const [q] = useState(() => new THREE.Vector4());
+  const [textureMatrix] = useState(() => new THREE.Matrix4());
+  const [virtuaCamera] = useState(() => new THREE.PerspectiveCamera());
+  const { gl, scene, camera } = useThree();
+
+  const beforeRender = useCallback(() => {
+    if (!meshRef.current) return;
+    meshRef.current.visible = false;
+  });
 }
