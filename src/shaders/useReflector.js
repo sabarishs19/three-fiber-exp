2da7e9eed0e2d56db1efa5b07b25cc681b0a25ec
sabarishs19 <84761863+sabarishs19@users.noreply.github.com>
2021-11-30 14:40:19 +0530

    state

--- src/shaders/useReflector.js
+++ src/shaders/useReflector.js
@@ -6,4 +6,10 @@ import { useThree } from "@react-three/fiber";
 
 export default function useReflector(textureWidth = 128, textureHeight = 128) {
   const meshRef = useRef();
+  const [reflectorPlane] = useState(() => new THREE.Plane());
+  const [normal] = useState(() => new THREE.Vector3());
+  const [reflectorWorlPosition] = useState(() => new THREE.Vector3());
+  const [cameraWorldPosition] = useState(() => new THREE.Vector3());
+  const [rotationMatrix] = useState(() => new THREE.Vector3());
+  const [lookAtPosition] = useState(() => new THREE.Vector3(0, 0, -1));
 }
