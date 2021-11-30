e788ad2a30fb18fc5b08b86e0190a090d67ed75c
sabarishs19 <84761863+sabarishs19@users.noreply.github.com>
2021-11-30 18:41:26 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,37 @@
 import "./styles.css";
+import React, { Suspense } from "react";
+import { Canvas } from "@react-three";
+import { Circle, OrbitCountrols, Environment } from "@react-three/drei";
+import usePostprocessing from "./shaders/usePostprocessing";
+import useReflector from "./shaders/useReflector";
+import Sphere from "./Sphere";
+import "./shaders/materials/ReflectorMaterial";
+
+function Modal(props) {
+  const [meshRef, reflectorProps, passes] = useReflector();
+  usePostprocessing(passes);
+  return (
+    <group {...props}>
+      <Sphere />
+      <Circle
+        receiveShadow
+        ref={meshRef}
+        args={[12.75, 36, 36]}
+        rotation-x={-Math.PI / 2}
+        position={[1, 0, 0]}
+      >
+        <reflectorMaterial
+          transparent
+          opacity={0.7}
+          color="black"
+          metalness={1}
+          roughness={1}
+          {...reflectorProps}
+        />
+      </Circle>
+    </group>
+  );
+}
 
 export default function App() {
   return (
